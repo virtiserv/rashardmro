b00be030e4f4a3059382c7f6a9ca1b6449c4e636
Rashard Iman Kelly @nasa-jpl <rashardkelly@proton.me>
2026-04-26 11:26:44 -0700

    @wellsfargo @whitehouse @nasa-jpl @blackgirlscode
    @nasa @foratlanta these are the Scripps and logs for the DNS audit downtown it includes the fraudulent mail servers that the bank is running off Akamai shared b*******

---
+++ assets/scripts/types.sh
@@ -0,0 +1,163 @@
+######################
+echo "CURRENT DNS MAP"
+echo " #NEWS https://www.cnn.com/2001/TECH/industry/09/11/akamai.founder/index.html"
+uname -a
+ifconfig 
+uptime
+cal
+nslookup -type=ns jpl.nasa.gov
+nslookup -type=mx jpl.nasa.gov
+nslookup -type=mx karenbass.com
+nslookup -type=ns karenbass.com
+nslookup -type=ns usgs.gov
+nslookup -type=mx usgs.gov
+nslookup -type=ns usps.com
+nslookup -type=ns usps.com
+nslookup -type=mx usps.com
+nslookup -type=ns wp1-ext.usps.gov
+nslookup -type=mx wp1-ext.usps.gov
+nslookup -type=mx lacourt.ca.gov
+nslookup -type=ns lacourt.ca.gov
+nslookup -type=ns lacourt.org
+nslookup -type=mx lacourt.org
+nslookup -type=mx nasa.gov
+nslookup -type=ns nasa.gov
+nslookup -type=ns flylax.com
+nslookup -type=mx flylax.com
+nslookup -type=mx cia.gov
+nslookup -type=ns cia.gov
+ traceroute google.com
+ nslookup -type=mx google.com     
+ nslookup -type=ns google.com
+ nslookup -type=ns fbi.gov
+ nslookup -type=mx fbi.gov
+ nslookup -type=mx epa.gov
+ nslookup -type=ns epa.gov
+ nslookup -type=ns lacoe.edu
+ nslookup -type=mx lacoe.edu
+ nslookup -type=ns jw.org
+ nslookup -type=mx jw.org
+nslookup -type=mx azbec.org
+nslookup -type=ns azbec.org
+nslookup -type=mx noaa.gov
+nslookup -type=ns noaa.gov
+nslookup -type=ns aia-aerospace.org
+nslookup -type=mx aia-aerospace.org
+nslookup -type=mx rocketlabcorp.com
+nslookup -type=mx rocketlabcorp.com
+nslookup -type=ns rtx.com
+nslookup -type=mx rtx.com
+nslookup -type=ns lacity.org
+nslookup -type=mx lacity.org
+nslookup -type=mx lacity.gov
+nslookup -type=ns lacity.gov
+nslookup -type=ns longbeach.gov
+nslookup -type=mx longbeach.gov
+nslookup -type=mx lapl.org
+nslookup -type=mx lapl.org
+nslookup -type=ns georgia.gov
+nslookup -type=mx georgia.gov
+nslookup -type=mx earthlink.net
+nslookup -type=ns earthlink.net
+nslookup -type=mx waterandpower.org
+nslookup -type=ns waterandpower.org
+nslookup -type=mx level3.net
+nslookup -type=ns level3.net
+nslookup -type=ns fultoncountyga.gov
+nslookup -type=mx fultoncountyga.gov
+nslookup -type=ns atlantaga.gov
+nslookup -type=mx atlantaga.gov
+nslookup -type=mx whitehouse.gov
+nslookup -type=mx whitehouse.gov
+nslookup -type=mx metro.net
+nslookup -type=ns metro.net
+nslookup -type=ns amtrack.com
+nslookup -type=mx amtrack.com
+nslookup -type=mx lapdonline.org
+nslookup -type=ns lapdonline.org
+traceroute lapdonline.org
+nslookup -type=ns lacounty.gov
+nslookup -type=mx lacounty.gov
+nslookup -type=ns commerce.gov
+nslookup -type=mx commerce.gov
+nslookup -type=ns polb.com
+nslookup -type=mx polb.com
+nslookup -type=mx  wellsfargo.com
+nslookup -type=ns  wellsfargo.com
+nslookup -type=ns musiccenter.org
+nslookup -type=mx musiccenter.org
+traceroute musiccenter.org
+nslookup -type=ns laopera.org 
+nslookup -type=mx laopera.org 
+traceroute laoperA.org
+nslookup -type=ns fire.ca.gov 
+nslookup -type=ns ca.gov
+nslookup -type=mx ca.gov
+nslookup -type=mx fire.ca.gov
+nslookup -type=ns stfranciscenterla.org   
+nslookup -type=mx stfranciscenterla.org   
+nslookup -type=mx state.gov
+
+nslookup -type=ns state.gov
+nslookup -type=ns cedars-sinai.org
+nslookup -type=mx cedars-sinai.org
+nslookup -type=ns embassyeritrea.org                 
+nslookup -type=mx embassyeritrea.org                   
+nslookup -type=ns ethiopianembassy.org                
+nslookup -type=mx ethiopianembassy.org                 
+nslookup -type=mx state.gov
+		        
+nslookup -type=ns embassyeritrea.org                  
+nslookup -type=mx stfranciscenterla.org                  
+nslookup -type=mx fire.ca.gov
+nslookup -type=ns ca.gov     
+nslookup -type=mx ca.gov     
+nslookup -type=mx  starbucks.com
+nslookup -type=ns starbucks.com
+nslookup -type=ns starbucks.com
+nslookup -type=mx commonspirit.org
+nslookup -type=ns commonspirit.org
+nslookup -type=ns dignityhealth.org
+nslookup -type=mx dignityhealth.org
+ping -c2 dignityhealth.org
+traceroute dignityhealth.org
+nslookup -type=ns starbucks.com
+traceroute  lacounty.gov
+ping -c2 lacounty.gov
+traceroute  fultoncountyga.gov
+ping -c2 fultoncountyga.gov
+traceroute  lacity.gov
+ping -c2 lacity.org
+ping -c2 lacity.gov
+traceroute  lacity.org
+traceroute longbeach.gov
+ping -c2 longbeach.gov
+traceroute whitehouse.gov
+ping -c2 whitehouse.gov
+traceroute nasa.gov
+ping -c2 nasa.gov
+ping -c2 lacourt.org
+traceroute lacourt.org
+ping -c2 lacourt.ca.gov
+traceroute lacourt.ca.gov
+ping -c1 state.gov 
+traceroute state.gov
+ping -c2 level3.net
+traceroute level3.net
+traceroute level1.net
+traceroute level2.net
+curl virtiserv.com
+curl nasa.gov
+curl whitehouse.gov
+nslookup -type=ns virtiserv.com
+nslookup -type=mx virtiserv.com
+#whois lacourt.ca.gov
+#whois lacourt.org
+#whois whitehouse.gov
+#whois commerce.gov
+#whois lacity.gov
+#whois lacity.org
+#whois lacounty.gov
+#westernunion
+#usajobs.gov nasaforce
+traceroute en.beidou.gov.cn
