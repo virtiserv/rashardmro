c8313f6a44b2ec38e3727bb35e417caf1a1c72da
Rashard Iman Kelly @nasa-jpl <rashardkelly@proton.me>
2026-05-18 10:01:41 -0700

    @la-county-isd u may need @nasa-jpl to call @whitehouse for instructions @blackgirlscode @linkedin @cityoflosangeles @stateofcalifornia
    @la-county-isd u may need @nasa-jpl to call @whitehouse for instructions @blackgirlscode @linkedin @cityoflosangeles @stateofcalifornia

---
+++ assets/logs/overviewtypes.sh
@@ -0,0 +1,268 @@
+#################⁸#####
+echo "CURRENT DNS MAP @la-county-isd @whitehouse @nasa-pds @blackgirlscode @fbicyber"
+date
+echo " #NEWS ra5hard.github.io @nasa-jpl  && cnn.com/2001/TECH/industry/09/11/akamai.founder/index.html"
+echo "https://archive.org/details/losangelesenormo0000weav #NEWS ra5hard.github.io  && cnn.com/2001/TECH/industry/09/11/akamai.founder/index.html"
+uname -a
+ifconfig 
+uptime
+date
+ulimit -a
+traceroute localhost
+cal
+nslookup -type=ns jpl.nasa.gov
+nslookup -type=mx jpl.nasa.gov
+nslookup -type=ns uahirise.org
+nslookup -type=mx uahirise.org
+nslookup -type=ns esa.int
+nslookup -type=mx esa.int
+nslookup -type=ns global.jaxa.jp
+nslookup -type=mx global.jaxa.jp
+nslookup -type=ns gov.uk
+nslookup -type=mx gov.uk
+dig whitehouse.gov NS +trace
+host whithouse.gov
+host trumpcard.gov
+host commerce.gov
+host usgs.gov
+host pbs.org
+nslookup -type=mx whitehouse.gov
+nslookup -type=ns whitehouse.gov
+nslookup -type=ns usps.com
+nslookup -type=mx usps.com
+dig usps.com ANY
+nslookup -type=ns wp1-ext.usps.gov
+nslookup -type=mx wp1-ext.usps.gov
+dig wp1-ext.usps.gov ANY
+nslookup -type=mx uspto.com
+nslookup -type=ns uspto.com
+host -a gov.uk
+host -a africanspaceagency.org
+host -a asal.dz
+host -a  mayor.lacity.gov
+host -a gavrt.lewiscenter.org
+host -a satellitegroundstation.com
+nslookup -type=ns jpl.nasa.gov
+nslookup -type=mx jpl.nasa.gov
+nslookup -type=ns space.gov.au
+nslookup -type=mx space.gov.au
+nslookup -type=mx congress.gov
+nslookup -type=ns congress.gov
+nslookup -type=mx dhs.gov
+nslookup -type=ns dhs.gov
+nslookup -type=mx si.edu
+nslookup -type=ns si.edu
+host -a si.edu
+nslookup -type=mx ice.gov
+nslookup -type=ns ice.gov
+nslookup -type=mx karenbass.com
+nslookup -type=ns karenbass.com
+nslookup -type=mx raulclaros.com
+nslookup -type=ns raulclaros.com
+host raulclaros.com
+traceroute jaxa.jp
+traceroute la28.org
+host la28.org
+nslookup -type=mx la28.org
+nslookup -type=ns la28.org
+nslookup -type=ns usgs.gov
+nslookup -type=mx usgs.gov
+dig fifa.com MX
+nslookup -type=mx bet.com
+
+nslookup -type=ns bet.com
+host bet.com
+host vsco.co
+nslookup -type=ns fb.com
+nslookup -type=mx fb.com
+nslookup -type=ns linkedin.com
+nslookup -type=mx linkedin.com
+nslookup -type=mx tiktok.com
+nslookup -type=ns tiktok.com
+nslookup -type=mx distrowatch.com
+nslookup -type=ns distrowatch.com
+host -a wikipedia.org
+host -a saxavord.com
+nslookup -type=ns facebook.com
+nslookup -type=mx facebook.com
+nslookup -type=ns x.com
+nslookup -type=mx x.com
+host twitter.com
+nslookup -type=ns tumblr.com
+nslookup -type=mx tumblr.com
+nslookup -type=ns essence.com
+nslookup -type=mx essence.com
+nslookup -type=ns ebony.com
+nslookup -type=mx ebony.com
+
+host -a bsky.app
+
+## channels
+nslookup -type=ns cbs.com
+nslookup -type=mx cbs.com
+nslookup -type=any cbs.com
+dig cbssports.com ALL
+dig cbssports.com ANY
+nslookup -type=ns nbc.com
+nslookup -type=mx nbc.com
+nslookup -type=any nbc.com
+dig nbcsports.com ALL
+nslookup -type=ns foxnews.com
+nslookup -type=mx foxnews.com
+nslookup -type=any foxnews.com
+host fox.com
+nslookup -type=ns abc.com
+nslookup -type=mx abc.com
+nslookup -type=any abc.com
+nslookup -type=mx lacourt.ca.gov
+nslookup -type=ns lacourt.ca.gov
+nslookup -type=ns lacourt.org
+nslookup -type=mx lacourt.org
+nslookup -type=mx nasa.gov
+nslookup -type=ns nasa.gov
+nslookup -type=ns flylax.com
+nslookup -type=mx flylax.com
+nslookup -type=mx cia.gov
+nslookup -type=ns cia.gov
+ traceroute google.com
+ nslookup -type=mx google.com     
+ nslookup -type=ns google.com
+ nslookup -type=ns fbi.gov
+ nslookup -type=mx fbi.gov
+ nslookup -type=mx epa.gov
+ nslookup -type=ns epa.gov
+ nslookup -type=ns lacoe.edu
+ nslookup -type=mx lacoe.edu
+ nslookup -type=ns jw.org
+ nslookup -type=mx jw.org
+nslookup -type=mx azbec.org
+nslookup -type=ns azbec.org
+nslookup -type=mx noaa.gov
+nslookup -type=ns noaa.gov
+nslookup -type=ns aia-aerospace.org
+nslookup -type=mx aia-aerospace.org
+nslookup -type=ns rocketlabcorp.com
+nslookup -type=mx rocketlabcorp.com
+nslookup -type=ns rtx.com
+nslookup -type=mx rtx.com
+nslookup -type=ns lacity.org
+nslookup -type=mx lacity.org
+nslookup -type=mx lacity.gov
+nslookup -type=ns lacity.gov
+nslookup -type=ns longbeach.gov
+nslookup -type=mx longbeach.gov
+nslookup -type=mx lapl.org
+nslookup -type=mx lapl.org
+nslookup -type=ns georgia.gov
+nslookup -type=mx georgia.gov
+nslookup -type=mx earthlink.net
+nslookup -type=ns earthlink.net
+nslookup -type=mx waterandpower.org
+nslookup -type=ns waterandpower.org
+nslookup -type=mx level3.net
+nslookup -type=ns level3.net
+nslookup -type=ns fultoncountyga.gov
+nslookup -type=mx fultoncountyga.gov
+nslookup -type=ns atlantaga.gov
+nslookup -type=mx atlantaga.gov
+nslookup -type=mx whitehouse.gov
+nslookup -type=ns whitehouse.gov
+nslookup -type=mx metro.net
+nslookup -type=ns metro.net
+nslookup -type=ns amtrack.com
+nslookup -type=mx amtrack.com
+nslookup -type=ns bigbluebus.com
+nslookup -type=mx ladottransit.com
+nslookup -type=ns bigbluebus.com
+nslookup -type=mx ladottransit.com
+nslookup -type=mx lapdonline.org
+traceroute ladottransit.com
+nslookup -type=ns lapdonline.org
+traceroute lapdonline.org
+nslookup -type=ns lacounty.gov
+nslookup -type=mx lacounty.gov
+nslookup -type=ns commerce.gov
+nslookup -type=mx commerce.gov
+nslookup -type=ns polb.com
+nslookup -type=mx polb.com
+nslookup -type=mx  wellsfargo.com
+nslookup -type=ns  wellsfargo.com
+nslookup -type=ns musiccenter.org
+nslookup -type=mx musiccenter.org
+traceroute musiccenter.org
+nslookup -type=ns laopera.org 
+nslookup -type=mx laopera.org 
+traceroute laoperA.org
+nslookup -type=ns fire.ca.gov 
+nslookup -type=ns ca.gov
+nslookup -type=mx ca.gov
+nslookup -type=mx fire.ca.gov
+nslookup -type=ns stfranciscenterla.org   
+nslookup -type=mx stfranciscenterla.org   
+nslookup -type=mx state.gov
+
+nslookup -type=ns state.gov
+nslookup -type=ns cedars-sinai.org
+nslookup -type=mx cedars-sinai.org
+nslookup -type=ns embassyeritrea.org                 
+nslookup -type=mx embassyeritrea.org                   
+nslookup -type=ns ethiopianembassy.org                
+nslookup -type=mx ethiopianembassy.org                 
+nslookup -type=mx state.gov
+		        
+nslookup -type=ns embassyeritrea.org                  
+nslookup -type=mx stfranciscenterla.org                  
+nslookup -type=mx fire.ca.gov
+nslookup -type=ns ca.gov     
+nslookup -type=mx ca.gov     
+nslookup -type=mx  starbucks.com
+nslookup -type=ns starbucks.com
+nslookup -type=ns starbucks.com
+nslookup -type=mx commonspirit.org
+nslookup -type=ns commonspirit.org
+nslookup -type=ns dignityhealth.org
+nslookup -type=mx dignityhealth.org
+ping -c2 dignityhealth.org
+traceroute dignityhealth.org
+nslookup -type=ns starbucks.com
+traceroute  lacounty.gov
+ping -c2 lacounty.gov
+traceroute  fultoncountyga.gov
+ping -c2 fultoncountyga.gov
+traceroute  lacity.gov
+ping -c2 lacity.org
+ping -c2 lacity.gov
+traceroute  lacity.org
+traceroute longbeach.gov
+ping -c2 longbeach.gov
+traceroute whitehouse.gov
+ping -c2 whitehouse.gov
+traceroute nasa.gov
+ping -c2 nasa.gov
+ping -c2 lacourt.org
+traceroute lacourt.org
+ping -c2 lacourt.ca.gov
+traceroute lacourt.ca.gov
+ping -c1 state.gov 
+traceroute state.gov
+ping -c2 level3.net
+traceroute level3.net
+traceroute level1.net
+traceroute level2.net
+curl virtiserv.com
+curl nasa.gov
+curl whitehouse.gov
+traceroute esa.int
+nslookup -type=ns virtiserv.com
+nslookup -type=mx virtiserv.com
+echo " #NEWS ra5hard.github.io  && cnn.com/2001/TECH/industry/09/11/akamai.founder/index.html"
+#whois lacourt.ca.gov
+#whois lacourt.org
+#whois whitehouse.gov
+#whois commerce.gov
+#whois lacity.gov
+#whois lacity.org
+#whois lacounty.gov
+#westernunion
+#usajobs.gov nasaforce
+traceroute en.beidou.gov.cn
